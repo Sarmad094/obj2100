0a2e42f0973eda9484b29b09c3e298d34ef32218
Sarmad <sarmad04@outlook.com>
2025-04-06 17:02:32 +0200

    First

---
+++ src/main/java/com/example/obj2100/Obj2100Application.java
@@ -0,0 +1,13 @@
+package com.example.obj2100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Obj2100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Obj2100Application.class, args);
+    }
+
+}
